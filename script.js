ccf6c8bc9f6ce5f11cbf2ce54614dcaf38fd8b72
Johann Carfantan <jojohann22550@gmail.com>
2024-02-18 16:04:54 +0100

    remove unused code

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-// const baseUrl = 'https://www.magic-ville.com/pics/pfou/mkm/'
-const baseUrlBig = 'https://www.magic-ville.com/pics/big/mkmFR/'
+const cardPreview = document.getElementById('cardPreview')
 
-// const cardPreview = document.getElementById('cardPreview')
-const cardBigPreview = document.getElementById('cardBigPreview')
-
+const baseUrl = 'https://www.magic-ville.com/pics/big/mkmFR/'
 const numberOfCardsInSet = 433
 const randomNumberInSet = Math.floor(Math.random() * numberOfCardsInSet) + 1
 
-console.log(`Card: ${randomNumberInSet}`)
-// cardPreview.src = baseUrl + randomNumberInSet.toString().padStart(3, '0') + '.jpg'
-cardBigPreview.src = baseUrlBig + randomNumberInSet.toString().padStart(3, '0') + '.jpg'
+cardPreview.src = baseUrl + randomNumberInSet.toString().padStart(3, '0') + '.jpg'
